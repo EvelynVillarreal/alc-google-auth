4d14fd195c99f76c37b5c84c6244e290ae45122d
EvelynVillarreal <ehvillarreal@espe.edu.ec>
2026-05-28 17:10:11 -0500

    chore: update API URL and Google Client ID for new deploy

--- View/script/config.js
+++ View/script/config.js
@@ -1,2 +1,2 @@
-window.API_BASE_URL = "https://american-latin-class.onrender.com";
-window.GOOGLE_CLIENT_ID = "CHANGE_ME_GOOGLE_CLIENT_ID";
+window.API_BASE_URL = "https://alc-google-auth.onrender.com";
+window.GOOGLE_CLIENT_ID = "755509255995-2rfrb6atevd6d64vmndkucsdh8qh19j6.apps.googleusercontent.com";
